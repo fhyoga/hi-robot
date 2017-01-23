3b941be31d790d8d23d6dff33f8a2b5d5145c284
strongfanfan <463428221@qq.com>
2017-01-23 20:37:46 +0800

    聊天机器人

---
+++ hi.js
@@ -0,0 +1,12 @@
+var http=require('http');
+var urlencode=require('./urlencode');
+
+
+var text=urlencode('哈哈哈哈');
+
+http.get('http://op.juhe.cn/robot/index?info='+text+'&dtype=&loc=&lon=&lat=&userid=&key=9c8e285d667ae5a359e961d1fe506ad2',function (res) {
+    res.setEncoding('utf8');
+    res.on('data', function(data) {
+        console.log(data);
+    });
+});
