67da37408a23bd3624c09287678cea3d4b6e9739
strongfanfan <463428221@qq.com>
2017-01-23 22:18:16 +0800

    bug fixes

--- bin/hi.js
+++ bin/hi.js
@@ -1,15 +1,21 @@
 #!/usr/bin/env node
 
-var http=require('http');
-var urlencode=require('./urlencode');
-var argv=process.argv.slice(2);
+var http = require('http');
+var urlencode = require('./urlencode');
+var argv = process.argv.slice(2);
 
 
-var text=urlencode(argv);
+var text = urlencode(argv);
+if (text) {
 
-http.get('http://op.juhe.cn/robot/index?info='+text+'&dtype=&loc=&lon=&lat=&userid=&key=9c8e285d667ae5a359e961d1fe506ad2',function (res) {
-    res.setEncoding('utf8');
-    res.on('data', function(data) {
-        console.log(data);
+
+    http.get('http://op.juhe.cn/robot/index?info=' + text + '&dtype=&loc=&lon=&lat=&userid=&key=9c8e285d667ae5a359e961d1fe506ad2', function (res) {
+        res.setEncoding('utf8');
+        res.on('data', function (data) {
+            var ob = JSON.parse(data);
+            console.log(ob.result.text)
+        });
     });
-});
+}else {
+    console.log('I am awake,你怎么不说话')
+}
