c0ef69ff756b67065e3d3e62c4d2d63419e49ea3
strongfanfan <463428221@qq.com>
2017-01-23 21:22:08 +0800

    cmd

--- bin/hi.js
+++ bin/hi.js
@@ -1,8 +1,9 @@
 var http=require('http');
 var urlencode=require('./urlencode');
+var argv=process.argv.slice(2);
 
 
-var text=urlencode('哈哈哈哈');
+var text=urlencode(argv);
 
 http.get('http://op.juhe.cn/robot/index?info='+text+'&dtype=&loc=&lon=&lat=&userid=&key=9c8e285d667ae5a359e961d1fe506ad2',function (res) {
     res.setEncoding('utf8');
