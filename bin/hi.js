84b0fc4e22baf5c4a19dcdd6cc35f43e5601afe1
strongfanfan <463428221@qq.com>
2017-02-01 18:42:18 +0800

    bug fixes

--- bin/hi.js
+++ bin/hi.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 var http = require('http');
-var urlencode = require('./urlencode');
+var urlencode = require('urlencode');
 var argv = process.argv.slice(2);
 
 
@@ -12,7 +12,7 @@ if (text) {
     http.get('http://op.juhe.cn/robot/index?info=' + text + '&dtype=&loc=&lon=&lat=&userid=&key=9c8e285d667ae5a359e961d1fe506ad2', function (res) {
         res.setEncoding('utf8');
         res.on('data', function (data) {
-            var ob = JSON.parse(data);
+            var ob = JSON.parse(data);  
             console.log(ob.result.text)
         });
     });
